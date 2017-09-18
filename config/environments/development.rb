770fa5a1c47d6467a4cad68de2f5de357ae99cab
hungnd <hungnd.k58@gmail.com>
2017-09-18 12:02:19 +0700

    signup,login ok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #begin cấu hình cho việc và gửi link trong mail xác nhận
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }  
+  #end
 end
---
+++ config/initializers/devise.rb
@@ -0,0 +1,11 @@
+# file này đc thêm để thay cái tên email trong thư xác nhận mà thôi: 
+Devise.setup do |config|
+  # The e-mail address that mail will appear to be sent from
+  # If absent, mail is sent from "please-change-me-at-config-initializers-devise@example.com"
+  config.mailer_sender = "bkteam@cmc.com.vn"
+
+  # If using rails-api, you may want to tell devise to not use ActionDispatch::Flash
+  # middleware b/c rails-api does not include it.
+  # See: http://stackoverflow.com/q/19600905/806956
+  config.navigational_formats = [:json]
+end
